7f71475ac3b632628746bf59ae1446896668e59f
Saleh Alkhalifa <alkhalifas@thallium.lan>
2024-01-31 19:04:10 -0500

    added UDP Client that extends abstract client

--- client/UDPClient.java
+++ client/UDPClient.java
@@ -1,1 +1,13 @@
 package client;
+
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.io.IOException;
+
+public class UDPClient extends AbstractClient {
+    public UDPClient(String hostname, int port) {
+        super(hostname, port);
+    }
+
+}
