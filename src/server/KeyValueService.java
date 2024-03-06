83c80b24fc3cb4de1f620729891c93581a1922e4
Saleh Alkhalifa <alkhalifas@thallium.lan>
2024-03-05 19:49:19 -0500

    modified the KV store to log data at the point where data is actually added to the store

--- src/server/KeyValueService.java
+++ src/server/KeyValueService.java
@@ -5,30 +5,32 @@ import java.rmi.RemoteException;
 import java.util.concurrent.ConcurrentHashMap;
 
 /*
- Key Val Service to extend UnicastRemoteObject that uses hashmap
+ Key Val Service to extend UnicastRemoteObject that uses hashmap with logging
  */
 public class KeyValueService extends UnicastRemoteObject implements KeyValueInterface {
     private ConcurrentHashMap<String, String> dataStore;
 
     public KeyValueService() throws RemoteException {
         super();
-
-        // double check in docs for best practice around the use of DS here
         dataStore = new ConcurrentHashMap<>();
     }
 
     @Override
     public void put(String key, String value) {
         dataStore.put(key, value);
+        ServerLogger.log("PUT operation - Key: " + key + ", Value: " + value);
     }
 
     @Override
     public String get(String key) {
-        return dataStore.get(key);
+        String value = dataStore.get(key);
+        ServerLogger.log("GET operation - Key: " + key + ", Value: " + value);
+        return value;
     }
 
     @Override
     public void delete(String key) {
         dataStore.remove(key);
+        ServerLogger.log("DELETE operation - Key: " + key);
     }
 }
