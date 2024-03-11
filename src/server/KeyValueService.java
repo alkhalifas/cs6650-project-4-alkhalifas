584166b50e8e410e30ee1516c430d84d49fcdf3a
Saleh Alkhalifa <alkhalifas@thallium.lan>
2024-03-11 19:33:03 -0400

    added docs to kv service

--- src/server/KeyValueService.java
+++ src/server/KeyValueService.java
@@ -10,17 +10,30 @@ import java.util.concurrent.ConcurrentHashMap;
 public class KeyValueService extends UnicastRemoteObject implements KeyValueInterface {
     private ConcurrentHashMap<String, String> dataStore;
 
+    /**
+     * Public of KeyValueService where datastore is instantiated
+     * @throws RemoteException
+     */
     public KeyValueService() throws RemoteException {
         super();
         dataStore = new ConcurrentHashMap<>();
     }
 
+    /**
+     * PUT method for the data store
+     * @param key
+     * @param value
+     */
     @Override
     public void put(String key, String value) {
         dataStore.put(key, value);
         ServerLogger.log("PUT operation - Key: " + key + ", Value: " + value);
     }
 
+    /**
+     * GET method for the data store
+     * @param key
+     */
     @Override
     public String get(String key) {
         String value = dataStore.get(key);
@@ -28,6 +41,10 @@ public class KeyValueService extends UnicastRemoteObject implements KeyValueInte
         return value;
     }
 
+    /**
+     * DELETE method for the data store
+     * @param key
+     */
     @Override
     public void delete(String key) {
         dataStore.remove(key);
