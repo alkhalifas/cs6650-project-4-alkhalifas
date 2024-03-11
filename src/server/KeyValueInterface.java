0b6aaa94ba231138312bc4faae3ac8a749f2cedf
Saleh Alkhalifa <alkhalifas@thallium.lan>
2024-03-11 19:34:39 -0400

    added docs to KVInterface

--- src/server/KeyValueInterface.java
+++ src/server/KeyValueInterface.java
@@ -8,7 +8,27 @@ Key Value Interface to extend the remote class from java, per the recommendation
 for project 2
  */
 public interface KeyValueInterface extends Remote {
+
+
+    /**
+     * PUT method for KeyValueInterface
+     * @param key
+     * @param value
+     * @throws RemoteException
+     */
     void put(String key, String value) throws RemoteException;
+
+    /**
+     * GET method for KeyValueInterface
+     * @param key
+     * @throws RemoteException
+     */
     String get(String key) throws RemoteException;
+
+    /**
+     * DELETE method for KeyValueInterface
+     * @param key
+     * @throws RemoteException
+     */
     void delete(String key) throws RemoteException;
 }
