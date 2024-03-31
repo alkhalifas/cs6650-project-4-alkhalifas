41ef288691080fe4224d04926ef2d5076aa3c3b8
Saleh Alkhalifa <alkhalifas@thallium.lan>
2024-03-31 12:42:42 -0400

    modified serverapp to have 5 KV services

--- src/server/ServerApp.java
+++ src/server/ServerApp.java
@@ -9,31 +9,41 @@ import java.rmi.registry.Registry;
 public class ServerApp {
     public static void main(String[] args) {
 
-        // Set default port
-        int port = 1099;
-
-        // If port given, use that instead
-        if (args.length > 0) {
-            try {
-                port = Integer.parseInt(args[0]);
-                ServerLogger.log("Port number provided, using port: " + port);
-            } catch (NumberFormatException e) {
-                ServerLogger.log("Invalid port number provided, using default: " + port);
-            }
-        }
-
         try {
-            // Crete a new instance of the service
-            KeyValueService service = new KeyValueService();
+            // Set default port
+            int port = 1099;
 
-            // Use 1099 which is default for rmi
-            Registry registry = LocateRegistry.createRegistry(port);
+            // If port given, use that instead
+            if (args.length > 0) {
+                try {
+                    port = Integer.parseInt(args[0]);
+                    ServerLogger.log("Port number provided, using port: " + port);
+                } catch (NumberFormatException e) {
+                    ServerLogger.log("Invalid port number provided, using default: " + port);
+                }
+            }
 
-            // Rebind registry
-            registry.rebind("KeyValueService", service);
+            // Create and bind instances of KeyValueService on multiple servers to adhere to reqs of prj3
+            // Prj3 new change
+            List<KeyValueService> services = new ArrayList<>();
 
-            // print for now
-            ServerLogger.log("Key-Value Store Service is running on port " + port);
+            // Iterate over the services to spin them up
+            for (int i = 0; i < 5; i++) {
+                // Create a new instance of the service
+                KeyValueService service = new KeyValueService();
+
+                // Use the specified port or default (1099)
+                Registry registry = LocateRegistry.createRegistry(port + i);
+
+                // Rebind registry
+                registry.rebind("KeyValueService", service);
+
+                // add it to the service
+                services.add(service);
+
+                // Log to the logger
+                ServerLogger.log("Key-Value Store Service is running on port " + (port + i));
+            }
         } catch (Exception e) {
             ServerLogger.log("Server exception: " + e.toString());
             e.printStackTrace();
