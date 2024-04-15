32bd243723870ffe54a3612be4d1572c0c623d2c
alkhalifas <salehesam@gmail.com>
2024-04-15 18:17:58 -0400

    added prepare and accept methods in Acceptor interface

--- src/server/IAcceptor.java
+++ src/server/IAcceptor.java
@@ -3,5 +3,6 @@ package node;
 import java.rmi.Remote;
 
 public interface IAcceptor extends Remote {
-
+  Object prepare(long sequenceNumber) throws RemoteException;
+  long accept(long sequenceNumber, Object proposalValue) throws RemoteException;
 }
