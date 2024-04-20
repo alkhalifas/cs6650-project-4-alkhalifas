d06b4ddade7cc1c6fd795cd4ff178aef2471a7e5
alkhalifas <salehesam@gmail.com>
2024-04-20 10:04:31 -0400

    added scripts for deployment

--- run_server_no_docker.sh
+++ run_server_no_docker.sh
@@ -2,8 +2,8 @@
 
 cd src
 
-rm -r coordinator/*.class server/*.class utils/*.class main/ServerMain.class
-javac coordinator/*.java server/*.java utils/*.java main/ServerMain.java
+rm -r server/*.class utils/*.class main/ServerMain.class
+javac server/*.java utils/*.java main/ServerMain.java
 
 # Run chmod +x run_server_no_docker.sh if needed
 java -cp . main.ServerMain
