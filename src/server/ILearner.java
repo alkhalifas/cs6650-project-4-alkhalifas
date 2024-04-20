ae3431a87f51477771ac1bfaf5476e7a672b1a8a
alkhalifas <salehesam@gmail.com>
2024-04-20 09:02:00 -0400

    updated docs in learner

--- src/server/ILearner.java
+++ src/server/ILearner.java
@@ -3,7 +3,18 @@ package server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * The ILearner interface defines the remote method that must be implemented by a learner
+ * in a consensus algorithm.
+ */
 public interface ILearner extends Remote {
 
+  /**
+   * Receives notification of an accepted value from acceptors, completing the consensus process.
+   *
+   * @param sequenceNumber The sequence number of the accepted proposal, ensuring the correct ordering of learned values.
+   * @param acceptedValue The value that has been accepted, which is now to be learned and acted upon.
+   * @throws RemoteException If an RMI error occurs during the remote method call.
+   */
   void learn(long sequenceNumber, Object acceptedValue) throws RemoteException;
 }
